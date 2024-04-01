e038d49b57e7280440a3a6dec87611144be9f0fa
Aktan Klimov <aktan.klimov2@gmail.com>
2024-04-01 19:18:41 +0600

    added new files

---
+++ src/main/resources/db/changelog/initial/005_fill_tables.sql
@@ -0,0 +1,38 @@
+--liquibase formatted sql
+--changeset Aktan: fill tables
+
+INSERT INTO users (username, password, email, enabled) VALUES
+('maria', '$2a$12$WB2YUbFcCN0tm44SBcKUjua9yiFBsfB3vW02IjuwzY7HGtlQIKzy2', 'maria@gmail.com', true),
+('johndoe', 'hashed_password2', 'john@mail.com', true),
+('alexx', 'hashed_password2', 'alex@mail.com', true);
+
+INSERT INTO quizzes (title, description, creator_username) VALUES
+('Quiz 1 Title', 'Description of Quiz 1', (select username from users where email like 'maria@gmail.com')),
+('Quiz 2 Title', 'Description of Quiz 2', (select username from users where email like 'john@mail.com'));
+
+INSERT INTO questions (quiz_id, question_text) VALUES
+((select id from quizzes where title like 'Quiz 1 Title'), 'First question of Quiz 1'),
+((select id from quizzes where title like 'Quiz 1 Title'), 'Second question of Quiz 1'),
+((select id from quizzes where title like 'Quiz 2 Title'), 'First question of Quiz 2');
+
+INSERT INTO options (question_id, option_text, is_correct) VALUES
+((select id from questions where question_text like 'First question of Quiz 1'), 'Option 1 for Question 1', true),
+((select id from questions where question_text like 'First question of Quiz 1'), 'Option 2 for Question 1', false),
+((select id from questions where question_text like 'Second question of Quiz 1'), 'Option 1 for Question 2', true),
+((select id from questions where question_text like 'First question of Quiz 2'), 'Option 1 for Question 3', false);
+
+INSERT INTO quiz_results (user_username, quiz_id, score) VALUES
+((select username from users where email like 'alex@mail.com'), (select id from quizzes where title like 'Quiz 1 Title'), 80),
+((select username from users where email like 'maria@gmail.com'), (select id from quizzes where title like 'Quiz 2 Title'), 90);
+
+INSERT INTO AUTHORITIES (ROLE)
+VALUES
+    ('DEFAULT'),
+    ('ADMIN');
+
+INSERT INTO USER_AUTHORITY (USER_USERNAME, AUTHORITY_ID) values
+('maria', (select id from AUTHORITIES where role like 'DEFAULT')),
+('johndoe', (select id from AUTHORITIES where role like 'DEFAULT')),
+('alexx', (select id from AUTHORITIES where role like 'DEFAULT'));
+
+
